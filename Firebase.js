d134069ce56ceffd0119b106051bb74e769eb4d8
Rishabh Srivastava <82817552+Rishabhsri374@users.noreply.github.com>
2021-08-20 12:27:31 +0530

    Add files via upload

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCuKFncuyq4SQrSdE4XAeQaug5kihgpmrY",
+    authDomain: "auth-demo-b0c9e.firebaseapp.com",
+    projectId: "auth-demo-b0c9e",
+    storageBucket: "auth-demo-b0c9e.appspot.com",
+    messagingSenderId: "915707025268",
+    appId: "1:915707025268:web:319a1496d55bc969f53e21",
+    measurementId: "G-S65XXYGQ2B"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
